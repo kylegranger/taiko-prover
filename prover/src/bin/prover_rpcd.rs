2951396b66bff0c57601c518c5834769998410ec
kylegranger <kyle.robert.granger@gmail.com>
2024-01-09 20:33:15 +0100

    cleanup

--- prover/src/bin/prover_rpcd.rs
+++ prover/src/bin/prover_rpcd.rs
@@ -20,55 +20,56 @@ pub(crate) struct ProverdConfig {
 
 #[tokio::main]
 async fn main() {
-    let config = ProverdConfig::parse();
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    println!("are we there yet?");
+    // let config = ProverdConfig::parse();
+    // env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let shared_state = SharedState::new(SharedState::random_worker_id(), config.lookup);
-    {
-        // start the http server
-        let h1 = serve(&shared_state, &config.bind);
+    // let shared_state = SharedState::new(SharedState::random_worker_id(), config.lookup);
+    // {
+    //     // start the http server
+    //     let h1 = serve(&shared_state, &config.bind);
 
-        // starts the duty cycle loop
-        let ctx = shared_state.clone();
-        // use a dedicated runtime for mixed async / heavy (blocking) compute
-        let rt = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-        let h2 = rt.spawn(async move {
-            loop {
-                let ctx = ctx.clone();
-                // enclose this call to catch panics which may
-                // occur due to network services
-                let _ = tokio::spawn(async move {
-                    log::debug!("task: duty_cycle");
-                    ctx.duty_cycle().await;
-                })
-                .await;
-                tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-            }
-        });
+    //     // starts the duty cycle loop
+    //     let ctx = shared_state.clone();
+    //     // use a dedicated runtime for mixed async / heavy (blocking) compute
+    //     let rt = tokio::runtime::Builder::new_multi_thread()
+    //         .enable_all()
+    //         .build()
+    //         .unwrap();
+    //     let h2 = rt.spawn(async move {
+    //         loop {
+    //             let ctx = ctx.clone();
+    //             // enclose this call to catch panics which may
+    //             // occur due to network services
+    //             let _ = tokio::spawn(async move {
+    //                 log::debug!("task: duty_cycle");
+    //                 ctx.duty_cycle().await;
+    //             })
+    //             .await;
+    //             tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    //         }
+    //     });
 
-        // this task loop makes sure to merge task results periodically
-        // even if this instance is busy with proving
-        let ctx = shared_state.clone();
-        let h3 = tokio::spawn(async move {
-            loop {
-                let ctx = ctx.clone();
-                // enclose this call to catch panics which may
-                // occur due to network services
-                let _ = tokio::spawn(async move {
-                    log::debug!("task: merge_tasks_from_peers");
-                    let _ = ctx.merge_tasks_from_peers().await;
-                })
-                .await;
-                tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-            }
-        });
+    //     // this task loop makes sure to merge task results periodically
+    //     // even if this instance is busy with proving
+    //     let ctx = shared_state.clone();
+    //     let h3 = tokio::spawn(async move {
+    //         loop {
+    //             let ctx = ctx.clone();
+    //             // enclose this call to catch panics which may
+    //             // occur due to network services
+    //             let _ = tokio::spawn(async move {
+    //                 log::debug!("task: merge_tasks_from_peers");
+    //                 let _ = ctx.merge_tasks_from_peers().await;
+    //             })
+    //             .await;
+    //             tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    //         }
+    //     });
 
-        // wait for all tasks
-        if tokio::try_join!(h1, h2, h3).is_err() {
-            panic!("unexpected task error");
-        }
-    }
+    //     // wait for all tasks
+    //     if tokio::try_join!(h1, h2, h3).is_err() {
+    //         panic!("unexpected task error");
+    //     }
+    // }
 }
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-// fn load_block(filename: &str) -> Result<Block> {}
-
-// mod prover;
-
-fn main() {
-    // let block = load_block("block.json").unwrap();
-    println!("Hello, world!");
-    // let proof = compute_proof();
-}
