6f9bc552f99152733fe1e97b916630489da6bccf
kylegranger <kyle.robert.granger@gmail.com>
2024-01-09 20:15:43 +0100

    first check-in of prover from taiko-chain

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// fn load_block(filename: &str) -> Result<Block> {}
+
+// mod prover;
+
+fn main() {
+    // let block = load_block("block.json").unwrap();
+    println!("Hello, world!");
+    // let proof = compute_proof();
+}
